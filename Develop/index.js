d35221d7f6962ae040ee8291d4de74add813dbc8
Nick <chargerfan428@aol.com>
2023-05-27 12:17:40 -0700

    added bootstrap functionality, html barebones, and questions for JS

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is your github username?',
+    'What is the title of your project?',
+    'What is a description of your project',
+    'How Would I install your project?',
+    'What is a use case for your proejct?',
+    'What kind of license are you using?',
+    'How Would I be able to contribute to your project?',
+    'How could I test your project?',
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
