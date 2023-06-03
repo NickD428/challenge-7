c58cbada1c78c3269e0bff6ec57c5a339a2a86f3
Nick <chargerfan428@aol.com>
2023-06-03 15:03:59 -0700

    added switch statement to return license link

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -42,8 +42,44 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+      break;
+    case 'Apache-2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'GPL-3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+      break;
+    case 'GNU-3.0':
+      return 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+      break;
+    case 'BSD 2':
+      return 'https://opensource.org/license/bsd-2-clause/';
+      break;
+    case 'BSD 3':
+      return 'https://opensource.org/license/BSD-3-clause/';
+      break;
+    case 'Boost 1.0':
+      return 'https://opensource.org/license/bsl-1-0/';
+      break;
+    case 'Creative Commons 1.0':
+      return 'https://creativecommons.org/licenses/by/1.0/';
+      break;
+    case 'Eclipse':
+      return 'https://opensource.org/license/epl-1-0/';
+      break;
+    case 'Mozilla':
+      return 'https://opensource.org/license/mpl-2-0/';
+      break;
+    case 'Unlicense':
+      return 'https://opensource.org/license/unlicense/';
+    default:
+      return '';
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
