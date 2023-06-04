f39f50a2a1004a0102464e7ad8452c18ae081c2c
Nick <chargerfan428@aol.com>
2023-06-04 13:48:39 -0700

    added missing license link section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,72 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch (license) {
-    case 'MIT':
-      return 'MIT License';
-      break;
-    case 'Apache-2.0':
-      return 'Apache 2.0 License';
-      break;
-    case 'GPL-3.0':
-      return 'GPL 3.0 License';
-      break;
-    case 'GNU-3.0':
-      return 'GNU 3.0 License';
-      break;
-    case 'BSD 2':
-      return 'BSD 2 License';
-      break;
-    case 'BSD 3':
-      return 'BSD 3 License';
-      break;
-    case 'Boost 1.0':
-      return 'Boost 1.0 License';
-      break;
-    case 'Creative Commons 1.0':
-      return 'Creative Commons 1.0 License';
-      break;
-    case 'Eclipse':
-      return 'Eclipse License';
-      break;
-    case 'Mozilla':
-      return 'Mozilla Public License';
-      break;
-    case 'Unlicense':
-      return 'The Unlicense';
-    default:
-      return '';
-  }
-}
-
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   switch (license) {
     case 'MIT':
-      return 'https://opensource.org/licenses/MIT';
-      break;
-    case 'Apache-2.0':
-      return 'https://opensource.org/licenses/Apache-2.0';
-      break;
-    case 'GPL-3.0':
-      return 'https://www.gnu.org/licenses/gpl-3.0';
-      break;
-    case 'GNU-3.0':
-      return 'https://www.gnu.org/licenses/gpl-3.0.en.html';
-      break;
-    case 'BSD 2':
-      return 'https://opensource.org/license/bsd-2-clause/';
-      break;
-    case 'BSD 3':
-      return 'https://opensource.org/license/BSD-3-clause/';
-      break;
-    case 'Boost 1.0':
-      return 'https://opensource.org/license/bsl-1-0/';
-      break;
-    case 'Creative Commons 1.0':
-      return 'https://creativecommons.org/licenses/by/1.0/';
+      return 'MIT License';
       break;
     case 'Eclipse':
       return 'https://opensource.org/license/epl-1-0/';
@@ -80,6 +21,8 @@ function renderLicenseLink(license) {
       return '';
   }
 }
+
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
